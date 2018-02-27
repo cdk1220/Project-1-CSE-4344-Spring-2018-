9192c94de5068ef06266ee3591f4501f5823aa63
Don Chalanga Kuruppu <donchalangakuruppu@Dons-iMac.local>
2018-02-26 23:33:05 -0600

    A class was created to handle the project requirements for the server side.

--- server.py
+++ server.py
@@ -12,3 +12,20 @@ import sys
 from urllib.parse import urlparse
 import glob
 import os
+
+class ProjectRequirements():
+    HOST = "127.0.0.1"        # Listening to local server
+    PORT = 8080               # Listening to 8080 by default
+
+    def initiation(self):
+        # Checking if user has provided the script with a desired port number
+        if len(sys.argv) > 1:
+            # Try to convert given value for port number to an integer
+            try:
+                PORT = int(sys.argv[1])
+            except:
+                print("Given port number not acceptable. Using port 8080\n")
+        else:
+            print("Port number not provided by user. Using 8080 for port.\n")
+            
+    
